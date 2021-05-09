c6c8c7b95503db151bbc08eb108adbf6b991432d
Milena Giachetti <giachettimilena@gmail.com>
2021-05-09 15:15:40 -0300

    Add base name to router

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>,

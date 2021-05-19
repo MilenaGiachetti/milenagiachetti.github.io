8e5ba87e32a02e1be55c15063103e54588f8e026
Milena Giachetti <giachettimilena@gmail.com>
2021-05-19 00:15:41 -0300

    Add new home bg - first approach

---
+++ src/Components/Clouds/CloudSmall.js
@@ -0,0 +1,18 @@
+function CloudSmall(props) {
+	return (
+        <svg
+            data-name="Capa 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 290 99"
+            {...props}
+        >
+            <title>{"cloud_small"}</title>
+            <path
+                d="M273.5 66h-124A16.5 16.5 0 01133 49.5 16.5 16.5 0 01149.5 33h94A16.5 16.5 0 00260 16.5 16.5 16.5 0 00243.5 0h-227A16.5 16.5 0 000 16.5 16.5 16.5 0 0016.5 33h83A16.5 16.5 0 01116 49.5 16.5 16.5 0 0199.5 66h-37A16.5 16.5 0 0046 82.5 16.5 16.5 0 0062.5 99h211A16.5 16.5 0 00290 82.5 16.5 16.5 0 00273.5 66z"
+                fill={props.color}
+            />
+        </svg>
+	)
+}
+
+export default CloudSmall;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,15 @@
 import classes from './Home.module.scss';
+
+import CloudBig from '../../Components/Clouds/CloudBig';
+import CloudMedium from '../../Components/Clouds/CloudMedium';
+import CloudSmall from '../../Components/Clouds/CloudSmall';
+
 import {Link} from 'react-router-dom';
 
 function Home() {
 	return (
         <section className={classes.Home}>
             <div className={classes.title}>
-                <p className={classes.titleGreeting}>hola! soy</p>
                 <h2>Milena Giachetti</h2>
                 <p className={classes.titlePosition}>Desarrolladora Web</p>
                 <a href="mailto:giachettimilena@gmail.com?Subject=Contacto%20desde%20portfolio" target="_blank" rel="noreferrer" className={classes.titleEmail}aria-label="Mail">giachettimilena@gmail.com</a>
@@ -15,6 +19,17 @@ function Home() {
                         <div className={classes.titleCtaArrow}></div>
                         <p className={classes.titleCtaContent}>Ver Proyectos</p>
                     </Link>
+                </div>
+            </div>
+            <div className={classes.background}>
+                <div className={classes.sunset}>
+                    <div className={classes.sun}></div>
+                    <CloudBig className={classes.cloudBig} color={'rgba(255, 255, 255, 0.5)'} id="1"/>
+                    <CloudBig className={classes.cloudBig} color={'rgba(255, 255, 255, 0.25)'} id="2"/>
+                    <CloudBig className={classes.cloudBig} color={'rgba(255, 255, 255, 0.75)'} id="3"/>
+                    <CloudBig className={classes.cloudBig} color={'rgba(255, 255, 255, 0.95)'} id="4"/>
+                    <CloudMedium className={classes.cloudMedium} color={'rgba(255, 255, 255, 0.25)'} id="4"/>
+                    <CloudSmall className={classes.cloudSmall} color={'rgba(255, 255, 255, 0.65)'} id="4"/>
                 </div>
             </div>
         </section>

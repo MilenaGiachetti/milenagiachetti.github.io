871e9f395d618f14dc464daf2a02fce4ec8dc8e0
Milena Giachetti <giachettimilena@gmail.com>
2021-07-04 21:51:24 -0300

    Update home model

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -11,9 +11,9 @@ function Home(props) {
                 <p className={classes.titlePosition}>Desarrolladora Web</p>
                 <a href="mailto:giachettimilena@gmail.com?Subject=Contacto%20desde%20portfolio" target="_blank" rel="noreferrer" className={classes.titleEmail}aria-label="Mail">giachettimilena@gmail.com</a>
             </div>
-            <Canvas camera={{ position: [3, 2, 4] }} gl={{ antialias: true }} className={classes.Canvas}>
+            <Canvas camera={{position: [3, 2, 4]}} gl={{antialias: true}} className={classes.Canvas}>
                 <ambientLight intensity={0.5} />
-                <spotLight intensity={0.8} color={0xFFFF99} position={[5, 5, 6]} />
+                <spotLight intensity={2} color={0xFFFF99} position={[10, 10, 11]} />
                 <Suspense fallback={null}>{<Porfolio />}</Suspense>
             </Canvas>
         </section>

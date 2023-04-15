14e6cc436316b01a28ea01569113e47db2bb2348
MilenaGiachetti <giachettimilena@gmail.com>
2023-04-14 21:24:21 -0300

    Remove previous files

--- src/pages/Projects/data/imagesData.js
+++
@@ -1,8 +0,0 @@
-import guifoScreen from './../../../assets/images/projectsScreens/screens/guifos.png';
-import mixamoBg from './../../../assets/images/projectsScreens/screens/mixamo.png';
-
-const images = {
-    guifoScreen, mixamoBg
-};
-
-export default images;
